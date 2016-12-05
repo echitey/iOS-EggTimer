bc11e9eb3e720eb2a98e993a1e25fd4bd41f3286
Yawo Echitey <Yawo@yawomac.local>
2016-12-05 12:22:33 -0200

    Initial Commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time: Int = 210
+    
+    var timer =  Timer()
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func play(_ sender: Any) {
+        if(time > 0){
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.gogo), userInfo: nil, repeats: true)
+        }
+    }
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+    }
+    @IBAction func reset(_ sender: Any) {
+        time = 210
+        timeLabel.text = String(time)
+        timer.invalidate()
+    }
+    @IBAction func menosTen(_ sender: Any) {
+        if(time >= 10) {
+            time = time - 10
+            timeLabel.text = String(time)
+        }
+    }
+    @IBAction func maisTen(_ sender: Any) {
+        time = time + 10
+        timeLabel.text = String(time)
+    }
+    
+    func gogo(){
+        time = time - 1
+        timeLabel.text = String(time)
+        if(time == 0){
+            timer.invalidate()
+        }
+        
+    }
 
 }
 
